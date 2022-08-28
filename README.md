88aa313ef9e3c51408645d5378d5ccfca33616c2
splitant <axel.depret.pro@gmail.com>
2022-08-28 02:28:21 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,17 @@
+# Drupal pro docker
+
+
+## About The Project
+
+The goal is to set up fastly a local Drupal project with docker environment for professional uses.
+
+## Getting Started
+
+### Installation
+
+   ```sh
+   make create-setup <project> <repo-git>
+   # Fill GITLAB_TOKEN in .env
+   make copy-files
+   make setup
+   ```
---
+++ docker.mk
@@ -0,0 +1,158 @@
+include .env
+
+default: up
+
+COMPOSER_ROOT ?= /var/www/html
+DRUPAL_ROOT ?= /var/www/html/web
+
+## help	:	Print commands help.
+.PHONY: help
+ifneq (,$(wildcard docker.mk))
+help : docker.mk
+	@sed -n 's/^##//p' $<
+else
+help : Makefile
+	@sed -n 's/^##//p' $<
+endif
+
+## up	:	Start up containers.
+.PHONY: up
+up:
+	@echo "Starting up containers for $(PROJECT_NAME)..."
+	docker-compose pull
+	docker-compose up -d --remove-orphans
+
+.PHONY: mutagen
+mutagen:
+	mutagen-compose up
+
+## down	:	Stop containers.
+.PHONY: down
+down: stop
+
+## start	:	Start containers without updating.
+.PHONY: start
+start:
+	@echo "Starting containers for $(PROJECT_NAME) from where you left off..."
+	@docker-compose start
+
+## stop	:	Stop containers.
+.PHONY: stop
+stop:
+	@echo "Stopping containers for $(PROJECT_NAME)..."
+	@docker-compose stop
+
+## prune	:	Remove containers and their volumes.
+##		You can optionally pass an argument with the service name to prune single container
+##		prune mariadb	: Prune `mariadb` container and remove its volumes.
+##		prune mariadb solr	: Prune `mariadb` and `solr` containers and remove their volumes.
+.PHONY: prune
+prune:
+	@echo "Removing containers for $(PROJECT_NAME)..."
+	@docker-compose down -v $(filter-out $@,$(MAKECMDGOALS))
+
+## ps	:	List running containers.
+.PHONY: ps
+ps:
+	@docker ps --filter name='$(PROJECT_NAME)*'
+
+## shell	:	Access `php` container via shell.
+##		You can optionally pass an argument with a service name to open a shell on the specified container
+.PHONY: shell
+shell:
+	docker exec -ti -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell docker ps --filter name='$(PROJECT_NAME)_$(or $(filter-out $@,$(MAKECMDGOALS)), 'php')' --format "{{ .ID }}") sh
+
+## composer	:	Executes `composer` command in a specified `COMPOSER_ROOT` directory (default is `/var/www/html`).
+##		To use "--flag" arguments include them in quotation marks.
+##		For example: make composer "update drupal/core --with-dependencies"
+.PHONY: composer
+composer:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") composer --working-dir=$(COMPOSER_ROOT) $(filter-out $@,$(MAKECMDGOALS))
+
+## drush	:	Executes `drush` command in a specified `DRUPAL_ROOT` directory (default is `/var/www/html/web`).
+##		To use "--flag" arguments include them in quotation marks.
+##		For example: make drush "watchdog:show --type=cron"
+.PHONY: drush
+drush:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) $(filter-out $@,$(MAKECMDGOALS))
+
+## logs	:	View containers logs.
+##		You can optinally pass an argument with the service name to limit logs
+##		logs php	: View `php` container logs.
+##		logs nginx php	: View `nginx` and `php` containers logs.
+.PHONY: logs
+logs:
+	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
+
+.PHONY: create-setup
+create-setup:
+##		For example: make create-setup "<project_name> <repo-git>"
+	cd ~/Desktop
+	cp drupal-pro-docker $(word 2, $(MAKECMDGOALS))-docker
+	cd $(word 2, $(MAKECMDGOALS))-docker
+	git clone $(word 3, $(MAKECMDGOALS))
+
+.PHONY: setup
+setup:
+	make gitlab-auth
+	make vendor
+	make drupal-install
+	make packages
+	make build
+
+.PHONY: pull
+pull:
+	make drush-cex
+	git -C ./project/ pull origin develop
+	make vendor
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) updatedb -y
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) config:import -y
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) locale:check
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) locale:update
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) cache:rebuild
+
+.PHONY: drush-cex
+drush-cex:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) config:export -y --destination=./export
+
+.PHONY: vendor
+vendor:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") composer --working-dir=$(COMPOSER_ROOT) install -o
+
+.PHONY: gitlab-auth
+gitlab-auth:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") composer --working-dir=$(COMPOSER_ROOT) config --auth gitlab-token.gitlab.choosit.com ${GITLAB_TOKEN} --no-ansi --no-interaction
+
+.PHONY: copy-files
+copy-files:
+	make copy-pre-commit
+	make copy-settings-php
+
+.PHONY: copy-pre-commit
+copy-pre-commit:
+	cp docker_utils/pre-commit ./project/.git/hooks/pre-commit
+
+.PHONY: copy-settings-php
+copy-settings-php:
+	cp docker_utils/default.settings.php ./project/web/sites/default/settings.php
+
+.PHONY: drupal-install
+drupal-install:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) site:install minimal -y --account-name=${INSTALL_ACCOUNT_NAME} --account-pass=${INSTALL_ACCOUNT_PASS} --account-mail=${INSTALL_ACCOUNT_MAIL} --existing-config
+
+.PHONY: packages
+packages:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_node' --format "{{ .ID }}") npm install
+
+.PHONY: build
+build:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_node' --format "{{ .ID }}") npm run build
+
+.PHONY: restore-dump
+restore-dump:
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) sql-drop 
+	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) sqlq --file=$(filter-out $@,$(MAKECMDGOALS))
+
+# https://stackoverflow.com/a/6273809/1826109
+%:
+	@:
